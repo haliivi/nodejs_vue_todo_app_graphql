9f1a4828f56042a68d7195f8513f032282819966
a.sotnichenko <haliivi@mail.ru>
2023-08-23 09:30:17 +0700

    1) Install express-graphql and graphql package. 2) Use middleware for GraphQL. 3) Create two files resolver and schema.

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const path = require('path')
 const sequelize = require('./utils/database')
 const express = require('express')
 const app = express()
-const todoRoutes = require('./routes/todo')
+const {graphqlHTTP} = require('express-graphql')
+const schema = require('./graphql/schema')
+const resolver = require('./graphql/resolver')
 const PORT = process.env.PORT || 3000
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use(express.json())
-
-app.use('/api/todo', todoRoutes)
-
+app.use(graphqlHTTP({
+    schema,
+    rootValue: resolver
+}))
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
